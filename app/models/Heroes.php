e9adffc609006cfa802fd368890fa3a56ccad086
Wild <rdykyy@gmail.com>
2015-02-15 17:21:28 +0200

    Fixed typo.

--- app/models/Heroes.php
+++ app/models/Heroes.php
@@ -45,6 +45,12 @@ class Heroes extends \Phalcon\Mvc\Model
      */
     protected $locationId;
 
+    /**
+     *
+     * @var integer
+     */
+    protected $xp;
+
     public function initialize(){
         $this->belongsTo("userId" , "Users"  , "id");
         $this->belongsTo("classId", "Classes", "id");
