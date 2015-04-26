1eeb1cbe59f29aeb6654846b51349d757655bd7a
ScienceTechnology <warcraft3>
2015-04-26 17:34:52 +0300

    Added axe skills

--- app/config/skills/warriorSkillsConfig.php
+++ app/config/skills/warriorSkillsConfig.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    //Sword skills
     1 => [
         'name' => 'Удар мечом',
         'requiredLevel' => 1,
@@ -57,6 +58,62 @@ return [
         'description' => "Вероятность нанести критический удар повышена на %d%%. Сила критического удара повышена на %d%%",
         'iconSrc' => ''
     ],
-
+    //Axe skills
+    8 => [
+        'name' => 'Удар топором',
+        'requiredLevel' => 1,
+        'requiredSkill' => null,
+        'type' => 'attack',
+        'description' => "Наносит по сопернику удар оружием типа <b>Топор</b> %d%% единиц урона.",
+        'iconSrc' => ''
+    ],
+    9 => [
+        'name' => 'Братство топора',
+        'requiredLevel' => 2,
+        'requiredSkill' => 8,
+        'type' => 'attack',
+        'description' => "Добавляет %d%% урона тем союзникам, у которых в руках оружие типа <b>Топор</b>",
+        'iconSrc' => ''
+    ],
+    10 => [
+        'name' => 'Рассечение',
+        'requiredLevel' => 5,
+        'requiredSkill' => 9,
+        'type' => 'attack',
+        'description' => "Наносит удар оружием типа <b>Топор</b>, после чего понижает атаку цели на %d%%",
+        'iconSrc' => ''
+    ],
+    11 => [
+        'name' => 'Берсерк',
+        'requiredLevel' => 6,
+        'requiredSkill' => 10,
+        'type' => 'attack',
+        'description' => "На %d%% ходов увеличивается показатель силы умения <b>Удар топором</b> на %d%%, теряя при ударе %d%% очков жизни",
+        'iconSrc' => ''
+    ],
+    12 => [
+        'name' => 'Круговой удар',
+        'requiredLevel' => 7,
+        'requiredSkill' => 11,
+        'type' => 'attack',
+        'description' => "Наносит всем соперникам <b>Удар топором</b>.",
+        'iconSrc' => ''
+    ],
+    13 => [
+        'name' => 'Удар по рукам',
+        'requiredLevel' => 9,
+        'requiredSkill' => 12,
+        'type' => 'attack',
+        'description' => "Выбранный враг на %d%% ходов не использует бонус оружия в руках",
+        'iconSrc' => ''
+    ],
+    14 => [
+        'name' => 'Звериный рёв',
+        'requiredLevel' => 10,
+        'requiredSkill' => 13,
+        'type' => 'attack',
+        'description' => "Увеличивает всей команде силу атаки на  %d%% и понижает соперникам броню на %d%%  на 2 хода",
+        'iconSrc' => ''
+    ],
 
 ];
