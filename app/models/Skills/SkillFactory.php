5053c8faef3297ea6e2848f77114c093c9199e2d
ScienceTechnology <warcraft3>
2015-05-04 19:34:58 +0300

    Updated skills list

--- app/models/Skills/SkillFactory.php
+++ app/models/Skills/SkillFactory.php
@@ -3,7 +3,20 @@
 class SkillFactory {
 
     public static $config = [
-        1 => 'SwordSkill'
+        1 => 'SwordSkill',
+        2 => 'HonorshipSkill',
+        3 => 'LeadershipSkill',
+        4 => 'RageSkill',
+        5 => 'SlidingStrikeSkill',
+        6 => 'ArmorBreakSkill',
+        7 => 'CritycalStrikeSkill',
+        8 => 'AxeSkill',
+        9 => 'AxeBrotherhoodSkill',
+        10 => 'DissectionSkill',
+        11 => 'BerserkSkill',
+        12 => 'CircularStrikeSkill',
+        13 => 'HandsStrikeSkill',
+        14 => 'AnimalRoarSkill'
     ];
 
     public static $_id = null;
